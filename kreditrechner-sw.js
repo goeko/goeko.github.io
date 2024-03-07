ca63d9ef21f0aa601dd6553b45b2d2e4e90d23f1
_goEko_ <goeko@goeko.de>
2024-03-08 00:14:15 +0100

    ...

---
+++ kreditrechner-sw.js
@@ -0,0 +1,59 @@
+// Dateiname: sw.js
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/kreditrechner.html',
+                '/assets/icons/android-chrome-192x192.png?v=2',
+                '/assets/icons/android-chrome-512x512.png?v=2',
+
+                'apple-touch-icon-144x144.png?v=2',
+                'apple-touch-icon-152x152-precomposed.png?v=2',
+                'apple-touch-icon-152x152.png?v=2',
+                'apple-touch-icon-180x180-precomposed.png?v=2',
+                'apple-touch-icon-180x180.png?v=2',
+                'apple-touch-icon-57x57-precomposed.png?v=2',
+                'apple-touch-icon-57x57.png?v=2',
+                'apple-touch-icon-60x60-precomposed.png?v=2',
+                'apple-touch-icon-60x60.png?v=2',
+                'apple-touch-icon-72x72-precomposed.png?v=2',
+                'apple-touch-icon-72x72.png?v=2',
+                'apple-touch-icon-76x76-precomposed.png?v=2',
+                'apple-touch-icon-76x76.png?v=2',
+                'apple-touch-icon-precomposed.png?v=2',
+                'apple-touch-icon.png?v=2',
+                'browserconfig.xml?v=2',
+                'favicon-16x16.png?v=2',
+                'favicon-32x32.png?v=2',
+                'favicon.ico?v=2',
+                'html_code.html?v=2',
+                'mstile-144x144.png?v=2',
+                'mstile-150x150.png?v=2',
+                'mstile-310x150.png?v=2',
+                'mstile-310x310.png?v=2',
+                'mstile-70x70.png?v=2',
+                'safari-pinned-tab.svg?v=2',
+                'site.webmanifest?v=2',
+                'android-chrome-192x192.png?v=2',
+                'android-chrome-512x512.png?v=2',
+                'apple-touch-icon-114x114-precomposed.png?v=2',
+                'apple-touch-icon-114x114.png?v=2',
+                'apple-touch-icon-120x120-precomposed.png?v=2',
+                'apple-touch-icon-120x120.png?v=2',
+                'apple-touch-icon-144x144-precomposed.png?v=2',
+
+
+
+        ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
